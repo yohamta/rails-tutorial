b338cd6f33e956b69b5eb8fffe6b31dc476168af
yh0m0d0 <yh0m0d0@gmail.com>
2018-04-29 05:22:18 +0000

    Delete foo controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,4 @@ Rails.application.routes.draw do
   get '/about', to:'static_pages#about'
   get '/contact', to:'static_pages#contact'
   get '/signup', to:'users#new'
-  get 'foo/bar'
-  get 'foo/baz'
 end
