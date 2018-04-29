d4954e50bb2a7cf32e33d7580d739672f1710d60
yh0m0d0 <yh0m0d0@gmail.com>
2018-04-29 03:59:46 +0000

    Add contact page

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,9 @@
 Rails.application.routes.draw do
-  get 'foo/bar'
-
-  get 'foo/baz'
-
-  get 'static_pages/home'
-
-  get 'static_pages/help'
-  
-  get 'static_pages/about'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
+  get 'foo/bar'
+  get 'foo/baz'
+  get 'static_pages/home'
+  get 'static_pages/help'
+  get 'static_pages/about'
+  get 'static_pages/contact'
 end
