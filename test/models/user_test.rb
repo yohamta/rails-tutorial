6d252cc3459066498e93dca8e97dca12d5306666
yh0m0d0 <yh0m0d0@gmail.com>
2018-04-29 07:08:31 +0000

    Add user model validation

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -50,4 +50,17 @@ class UserTest < ActiveSupport::TestCase
       assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
     end
   end
+  
+  test "email addresses should be unique" do
+    duplicate_user = @user.dup
+    @user.save
+    assert_not duplicate_user.valid?
+  end
+  
+  test "email addresses should be saved as lower-case" do
+    mixed_case_email = "Foo@ExAMPle.CoM"
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
+  end
 end
