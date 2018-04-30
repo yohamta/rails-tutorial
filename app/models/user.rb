e87bd42468229824300e7827f8f68b3aab7ccb5b
yh0m0d0 <yh0m0d0@gmail.com>
2018-04-30 03:48:54 +0000

    Add user login integration test

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,11 @@ class User < ApplicationRecord
     length: { minimum: 6 }
   
   has_secure_password
+  
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? 
+      BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+  
 end
