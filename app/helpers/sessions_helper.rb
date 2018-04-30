efc17f771b6a9075c21bac817408b1b45071504b
yh0m0d0 <yh0m0d0@gmail.com>
2018-04-30 09:18:07 +0000

    Improve edit feature

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -10,6 +10,10 @@ module SessionsHelper
     cookies.permanent[:remember_token] = user.remember_token
   end
   
+  def current_user?(user)
+    user == current_user
+  end
+  
   def current_user
     if (user_id = session[:user_id])
       @current_user ||= User.find_by(id: user_id)
@@ -20,7 +24,6 @@ module SessionsHelper
         @current_user = user
       end
     end
-      
     @current_user ||= User.find_by(id: session[:user_id])
   end
   
