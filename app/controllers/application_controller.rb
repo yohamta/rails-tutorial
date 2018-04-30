efdab1bfffb71ea139bb2ab0cbaf5bd3947957a8
yh0m0d0 <yh0m0d0@gmail.com>
2018-04-30 02:54:05 +0000

    Add login feature

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  def hello
-    render html: "hello, sample app!"
-  end
+  include SessionsHelper
 end
+
---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+  def new
+  end
+  
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # login success and redirects to user profile page
+      log_in user
+      redirect_to user
+    else
+      # error
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new'
+    end
+  end
+  
+  def destroy
+  end
+end
