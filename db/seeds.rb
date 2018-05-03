a9be121f6773ec6eb796c860c41fda1cc554110d
yh0m0d0 <yh0m0d0@gmail.com>
2018-05-03 02:12:50 +0000

    Add feature to delete user on users page

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!(
+  name: "yh0m0d0",
+  email: "yh0m0d0@gmail.com",
+  password: "foobar",
+  password_confirmation: "foobar",
+  admin: true)
+  
+99.times do |n|
+  name = Faker::Name.name
+  email = "example-#{n+1}@railstutorial.org"
+  password = "password"
+  User.create!(
+    name: name,
+    email: email,
+    password: password,
+    password_confirmation: password
+  )
+end
+  
