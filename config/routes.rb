e0a5370a94dec55c7cffd29e386161abc76faafe
yh0m0d0 <yh0m0d0@gmail.com>
2018-05-03 09:18:37 +0000

    Add Micropost test

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   get 'password_resets/new'
-
   get 'password_resets/edit'
-
   root 'static_pages#home'
   get '/help', to:'static_pages#help'
   get '/about', to:'static_pages#about'
@@ -12,6 +10,7 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
   post '/signup', to:'users#create'
+  get '/microposts', to: 'static_pages#home'
   resources :users
   resources :account_activations, only: [:edit]
   resources :password_resets, only: [:new, :create, :edit, :update]
